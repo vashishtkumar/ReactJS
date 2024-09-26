6853c7567d094746037e0bf89b5047404c00f516
vashishtkumar <vashishtk270@gmail.com>
2024-09-27 02:23:59 +0530

    First_impression of react app and rewriting app.jsx

---
+++ my_first_react_app/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Hello vashisht ! . I am Learning ReactJs</h1>
+      <button>click here</button>
+    </>
+  )
+}
+
+export default App
