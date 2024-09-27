d197b9169bca2f0d825c77f1a08aba3d6090c125
vashishtkumar <vashishtk270@gmail.com>
2024-09-27 21:01:52 +0530

    our !st component

--- my_first_react_app/src/App.jsx
+++ my_first_react_app/src/App.jsx
@@ -1,10 +1,23 @@
 import './App.css'
 
+function Title(){
+  return <h1>Hello Vashist !. This is title</h1>
+}
+function Header(){
+  return (
+    <>
+    <h2>Header section is there</h2>
+    <p>this is header section</p>
+    </>
+  )
+}
 function App() {
   
 
   return (
     <>
+    <Title/>
+    <Header></Header>
       <h1>Hello vashisht ! . I am Learning ReactJs</h1>
       <button>click here</button>
     </>
