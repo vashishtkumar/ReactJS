81d6dc6efeb4e8d721b12b37da798bded0f24359
vashishtkumar <vashishtk270@gmail.com>
2024-09-29 03:32:42 +0530

    Dynamic component styling

--- my_first_react_app/src/product.jsx
+++ my_first_react_app/src/product.jsx
@@ -1,11 +1,13 @@
 import photo from "./assets/photo.jpg"
 import "./product.css"
 export default function Product(props){
+    let res=props.price > 30000;
+    let styles={backgroundColor:res ? "pink":"purple"}
    return(
-        <div className="Product">
+        <div className="Product" style={styles}>
         <h1>{props.title}</h1>
         <h3>{props.desc}</h3>
-        {props.price> 30000 ? <p>discount of {10/2} %</p>:<a href="#">click for discount</a>}
+        {res > 30000 ? <p>discount of {10/2} %</p>:<a href="#">click for discount</a>}
         </div>
     )
    
