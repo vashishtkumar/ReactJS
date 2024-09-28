e02d4c04cf54416d1fb40486c3408e2f937d653d
vashishtkumar <vashishtk270@gmail.com>
2024-09-29 01:08:31 +0530

    passing arrays and objects

--- my_first_react_app/src/productTab.jsx
+++ my_first_react_app/src/productTab.jsx
@@ -1,11 +1,12 @@
 import Product from "./product";
-
+let feautres2={"angry":20, mood:"good" }
 function MulProduct(){
+    let feautres=["cute " ,"loving ","nature lover"]
 return(
     <>
-    <Product title="Cute Girl" cuteness="100"/>
-    <Product title="Lovely Girl" cuteness={200}></Product>
-    <Product title="inoccent girl"/>
+    <Product title="Cute Girl" cuteness="100" feautres={feautres} feautres2={feautres2}/>
+    <Product title="Lovely Girl" cuteness={200} feautres={feautres} feautres2={feautres2}></Product>
+    <Product title="inoccent girl" feautres={feautres} feautres2={feautres2}/>
     </>
 )
 }
