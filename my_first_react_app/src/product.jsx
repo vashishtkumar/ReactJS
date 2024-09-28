5763174c227d55447761d8e03121c20c2b3def12
vashishtkumar <vashishtk270@gmail.com>
2024-09-29 00:36:41 +0530

    component styling

--- my_first_react_app/src/product.jsx
+++ my_first_react_app/src/product.jsx
@@ -1,10 +1,11 @@
 import photo from "./assets/photo.jpg"
+import "./product.css"
 export default function Product(){
     return (
-        <>
+        <div className="Product">
         <h1>product</h1>
         <h3>product description</h3>
         <img src={photo} alt="a cute baby" className="photo"></img>
-        </>
+        </div>
     )
 }
