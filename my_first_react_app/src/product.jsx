61aff4af7cc52de6640289bc34e36981fe4b5655
vashishtkumar <vashishtk270@gmail.com>
2024-09-29 02:47:54 +0530

    conditional rendering part -2

--- my_first_react_app/src/product.jsx
+++ my_first_react_app/src/product.jsx
@@ -1,21 +1,13 @@
 import photo from "./assets/photo.jpg"
 import "./product.css"
 export default function Product(props){
-    
-    if(props.price > 30000){
+    let result=props.price > 30000 ? "discount of 5 %":"";
     return (
         <div className="Product">
         <h1>{props.title}</h1>
         <h3>{props.desc}</h3>
-        <p>discount of 5%</p>
-        </div>
-    )}
-    else
-    return(
-    <div className="Product">
-        <h1>{props.title}</h1>
-        <h3>{props.desc}</h3>
-        <a href="#">click to get discount</a>
+        <p>{result}</p>
         </div>
     )
+   
 }
