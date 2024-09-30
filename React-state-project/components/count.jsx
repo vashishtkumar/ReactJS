1ccc7e21360afd5f60522842306d22804dced0da
vashishtkumar <vashishtk270@gmail.com>
2024-09-30 23:45:56 +0530

    useState hook

--- React-state-project/components/count.jsx
+++ React-state-project/components/count.jsx
@@ -1,15 +1,24 @@
-export default function Count(){
-    let count=0;
+import {useState} from "react";
 
-    function increament(){
-        count+=1;
-console.log(count);
-    }
+export default function Count(){
+    
+let arr=useState(10);
+console.log(arr); // it will return an array exactly have two values
+
+let [StateVariable,setStateVariable]=useState(0);
+
+// state for counter
+let [Count,setCount]=useState(0);
+
+let increment=()=>{
+setCount(Count+1);
+console.log(Count);
+}
     
     return (
         <>
-        <h1>count = {count}</h1>
-       <button onClick={increament}>click me to increase</button>
+        <h1>count = {Count}</h1>
+       <button onClick={increment}>click me to increase</button>
         </>
     )
 } 
