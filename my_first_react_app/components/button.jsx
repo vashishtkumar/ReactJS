df9a1bf1dc640c4c3ee30302fe34ac6e4ea82d14
vashishtkumar <vashishtk270@gmail.com>
2024-09-30 12:21:17 +0530

    handling non click events

--- my_first_react_app/components/button.jsx
+++ my_first_react_app/components/button.jsx
@@ -1,16 +1,21 @@
-function Print(){
-    console.log("An event has been occured");
+function handleClick(){
+    console.log("clciked");
 }
 
 
-function demo(){
-console.log("bye!");
+let handleMoueOver=()=>{
+    console.log("mouse hovered");
 }
 
+let handleDoubleClick=function(){
+    console.log("double click");
+}
 export default function Button(){
 return (<>
-    <button onClick={Print}>click</button>
-    <p onClick={demo}> click me to chnage content</p>
+    <button onClick={handleClick}>click</button>
+    <p onClick={handleClick}> click me to chnage content</p>
+    <p onMouseOver={handleMoueOver}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint exercitationem ad architecto saepe nisi voluptatum libero optio nihil, praesentium odio cum accusamus dolor, quidem adipisci quaerat quasi ab sed culpa.</p>
+    <button onDoubleClick={handleDoubleClick}>Double click me remember donot use doubleclick()</button>
     </>
 )
 }
