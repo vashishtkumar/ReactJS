90a6edbd7e61d0a27f92de29c32dbf0cc8b296a8
vashishtkumar <vashishtk270@gmail.com>
2024-10-01 00:32:05 +0530

    Like button with number of clicks

--- React-state-project/components/Liked.jsx
+++ React-state-project/components/Liked.jsx
@@ -5,16 +5,21 @@ function LikedButton(){
     let styles={
         color:"red",
     }
+
 let [liked,setLiked]=useState(false);
+
+const [click,setClick]=useState(0);
 
 function handleLikeBtn(){
     setLiked(!liked);
+    setClick(click+1);
     console.log(liked);
 }
 
 return(
 <div>
-{
+<h2>clicks={click}</h2>
+{   
     liked?
     <i class="fa-solid fa-heart" onClick={handleLikeBtn} style={styles}></i>:
     <i className="fa-regular fa-heart" onClick={handleLikeBtn} ></i>
