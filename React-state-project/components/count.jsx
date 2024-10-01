c7b1d34a58513625c060260e0c9a86e35d130d6d
vashishtkumar <vashishtk270@gmail.com>
2024-10-01 11:56:40 +0530

    Some More Concept about State

--- React-state-project/components/count.jsx
+++ React-state-project/components/count.jsx
@@ -1,21 +1,24 @@
 import {useState} from "react";
 
 export default function Count(){
-    
-// state for counter
-let [count,setCount]=useState(0);
-console.log(`count after rendering ${count}`);
 
+let init=() =>{
+    console.log("init called again");
+    return Math.random();
+}
+// state for counter
+let [count,setCount]=useState(init);   // if you want to call only once then use init and if init is executed every time then init();
+console.log("re - render occurs");
 let increment=()=>{
 
+   // setCount(25);                      // so here only once rerender occurs
+    setCount(count+1);
     //setCount(count+1);
-    //setCount(count+1);
-setCount((currentValue)=>{  // call back in state function
-   return currentValue+1});  // return is must
-setCount((currentValue)=>{
-   return currentValue+1});  // return  is must
+// setCount((currentValue)=>{  // call back in state function
+//    return currentValue+1});  // return is must
+// setCount((currentValue)=>{
+//    return currentValue+1});  // return  is must
 
-console.log(`inner count =${count}`);
 }
     
     return (
