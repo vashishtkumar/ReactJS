e78c3f93c2919839ed28c906377d622e36b8bbb7
vashishtkumar <vashishtk270@gmail.com>
2024-10-02 15:25:46 +0530

    Array_state

--- React-state-part2/componenets/LudoButton.jsx
+++ React-state-part2/componenets/LudoButton.jsx
@@ -3,6 +3,7 @@ import { useState } from "react"
 export default function LudoButton(){
     
 const [moves,setMoves]=useState({blue:0,yellow:0,red:0,green:0});
+const [arr,setArr]=useState(["no moves "]);
 
 function handleBlue(){
     moves.blue=moves.blue+1;
@@ -13,7 +14,17 @@ function handleBlue(){
 }
 
 function handleYellow(){
-   setMoves({...moves,yellow:moves.yellow+1});
+   //setMoves({...moves,yellow:moves.yellow+1});
+   //arr.push("moves ");
+   // setArr([...arr]);   it can be used as callback as well
+
+   setArr((prevVal)=>{
+      return [...arr,"moves "];
+   })
+
+
+    console.log(arr);
+
 }
    function handleGreen(){
     setMoves({...moves,green:moves.green+1});
@@ -36,6 +47,7 @@ function handleYellow(){
                 <h2>Blue Moves:{moves.blue}</h2>
                 <button style={{backgroundColor:"blue" ,color:"black"}} onClick={handleBlue}>+1</button>
                 <h2>Yellow Moves:{moves.yellow}</h2>
+                <p>{arr}</p>
                 <button style={{backgroundColor:"yellow" ,color:"black"}}onClick={handleYellow}>+1</button>
                 <h2>Green Moves:{moves.green}</h2>
                 <button style={{backgroundColor:"green" ,color:"black"}}onClick={handleGreen}>+1</button>
